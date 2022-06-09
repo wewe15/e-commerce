2244b45159b3db36ea2000e545479cfd1bda896f
wewe15 <ahm.sami.271@gmail.com>
2022-06-09 04:47:04 +0200

    update config.js to heroku

--- config/config.js
+++ config/config.js
@@ -20,13 +20,20 @@ module.exports = {
       "password": null,
       "database": "database_test",
       "host": "127.0.0.1",
-      "dialect": "mysql"
+      "dialect": "postgres"
   },
   "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": process.env.DB_HOST,
+      "dialect": "postgres",
+      "logging": false,
+      "dialectOptions": {
+        "ssl": {
+          "require": true,
+          "rejectUnauthorized": false
+        }
+      }
   }
 };
